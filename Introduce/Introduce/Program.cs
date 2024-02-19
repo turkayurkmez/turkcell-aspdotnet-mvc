a881558b813d5586b747807f71a669254a5b962a
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-02-19 15:22:09 +0300

    Last changes an completed intoduce project

--- Introduce/Introduce/Program.cs
+++ Introduce/Introduce/Program.cs
@@ -4,9 +4,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 //app.MapGet("/", () => "Merhaba!");
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
 
